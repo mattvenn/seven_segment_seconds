6e14ad251faa3e54f2a7390798cd58818749100c
Matt Venn <matt@mattvenn.net>
2022-07-27 16:49:41 +0200

    minor updates for mpw7

--- config.tcl
+++ config.tcl
@@ -4,14 +4,18 @@ set ::env(DESIGN_NAME) seven_segment_seconds
 # Change if needed
 set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.v]
 
+# Core & Max layer
 set ::env(DESIGN_IS_CORE) 0
 set ::env(FP_PDN_CORE_RING) 0
-set ::env(GLB_RT_MAXLAYER) 5
+set ::env(RT_MAX_LAYER) {met4}
 
-# Fill this
-# 50 Mhz
+# Don't run CVC, avoid a warning
+set ::env(RUN_CVC) 0
+
+# Clock - 50 Mhz
 set ::env(CLOCK_PERIOD) "20"
 set ::env(CLOCK_PORT) "clk"
 
+# Density
 set ::env(FP_CORE_UTIL) 25
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
